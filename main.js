56659adc20db5a46823c3afa3c2dd76e6e16b553
s4gen <87096163+s4gen@users.noreply.github.com>
2022-03-13 19:05:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ let word = WORDS[Math.floor(Math.random()*1682)]
 console.log(word)
 const keys = document.getElementsByClassName("letter")
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-import JSConfetti from 'js-confetti'
 const jsConfetti = new JSConfetti()
 function contains(a, obj) {
     for (let i = 0; i < a.length; i++) {
