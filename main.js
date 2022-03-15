f8f7b011543934d4d1e85d8e6ee2602da15cc095
s4gen <87096163+s4gen@users.noreply.github.com>
2022-03-15 10:57:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ let word = WORDS[Math.floor(Math.random()*1682)]
 console.log(word)
 const keys = document.getElementsByClassName("letter")
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-import 'animate.css'
 const jsConfetti = new JSConfetti()
 const answer = document.getElementById("answer")
 
