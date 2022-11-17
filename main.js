ede4ef81bf4eeab2254ef13804236e8073804a98
s4gen <87096163+s4gen@users.noreply.github.com>
2022-11-17 10:35:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,41 @@
 import WORDS from './words.js'
 const jsConfetti = new JSConfetti()
 
+const delay = 500
+// Sequential touches must be in close vicinity
+const minZoomTouchDelta = 10
+
+// Track state of the last touch
+let lastTapAt = 0
+let lastClientX = 0
+let lastClientY = 0
+
+export default function preventDoubleTapZoom(event) {
+  // Exit early if this involves more than one finger (e.g. pinch to zoom)
+  if (event.touches.length > 1) {
+    return
+  }
+
+  const tapAt = new Date().getTime()
+  const timeDiff = tapAt - lastTapAt
+  const { clientX, clientY } = event.touches[0]
+  const xDiff = Math.abs(lastClientX - clientX)
+  const yDiff = Math.abs(lastClientY - clientY)
+  if (
+    xDiff < minZoomTouchDelta &&
+    yDiff < minZoomTouchDelta &&
+    event.touches.length === 1 &&
+    timeDiff < delay
+  ) {
+    event.preventDefault()
+    // Trigger a fake click for the tap we just prevented
+    event.target.click()
+  }
+  lastClientX = clientX
+  lastClientY = clientY
+  lastTapAt = tapAt
+}
+
 // Game
 let word = WORDS[Math.floor(Math.random()*1682)] // Randomly chosen word.
 let selectedRow = 'a' // The current row.
