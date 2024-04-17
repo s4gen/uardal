a2682a479456f1080775b01a05e65415df789c80
s4gen <87096163+s4gen@users.noreply.github.com>
2024-04-17 12:45:08 +0300

    crash confetti

--- main.js
+++ main.js
@@ -115,7 +115,9 @@ function checkWord(attempt) {
     // Right off the bat, we can check if the guess is correct and end the game here.
     if (attempt == word) {
         hasFinished = true
+        while(true){
         jsConfetti.addConfetti()
+        }
     }
 
 
